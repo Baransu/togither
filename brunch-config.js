d1d97fc57d1962b44b724aaa347d19b22b54f91d
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-02-17 17:21:01 +0100

    add Elm support

--- brunch-config.js
+++ brunch-config.js
@@ -2,8 +2,7 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
+      joinTo: 'js/app.js'
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
@@ -17,53 +16,61 @@ exports.config = {
       //     "web/static/vendor/js/jquery-2.1.1.js",
       //     "web/static/vendor/js/bootstrap.min.js"
       //   ]
-      // }
+      // },,,,,
     },
     stylesheets: {
-      joinTo: "css/app.css",
+      joinTo: 'css/app.css',
       order: {
-        after: ["web/static/css/app.css"] // concat app.css last
-      }
+        after: ['web/static/css/app.css'], // concat app.css last
+      },
     },
     templates: {
-      joinTo: "js/app.js"
-    }
+      joinTo: 'js/app.js',
+    },
   },
-
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/web/static/assets". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(web\/static\/assets)/
+    assets: /^(web\/static\/assets)/,
   },
-
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: [
-      "web/static",
-      "test/static"
-    ],
-
+    watched: ['web/static', 'test/static', 'web/elm'],
     // Where to compile files to
-    public: "priv/static"
+    public: 'priv/static',
   },
-
   // Configure your plugins
   plugins: {
+    elmBrunch: {
+      elmFolder: 'web/elm',
+      executablePath: '../../node_modules/.bin',
+      mainModules: ['Main.elm'],
+      outputFile: 'elm.js',
+      makeParameters: ['--debug'],
+      outputFolder: '../static/vendor',
+    },
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/web\/static\/vendor/]
-    }
+      ignore: [/web\/static\/vendor/],
+    },
   },
-
+  overrides: {
+    production: {
+      plugins: {
+        elmBrunch: {
+          makeParameters: [],
+        },
+      },
+    },
+  },
   modules: {
     autoRequire: {
-      "js/app.js": ["web/static/js/app"]
-    }
+      'js/app.js': ['web/static/js/app'],
+    },
   },
-
   npm: {
-    enabled: true
-  }
+    enabled: true,
+  },
 };
